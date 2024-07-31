6cab0b1df333dbe3faf723209b6a220ab7795098
Valery Stepanova <kvetkavaloshka@gmail.com>
2024-07-31 17:11:23 +0300

    configuration with java code; using jsp and thymeleaf templates

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DemoController {
-
-    @GetMapping(path = "/demo")
-    public String showPage(){
-        return "demo";
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/demo.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 37529
+  Date: 31.07.2024
+  Time: 16:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>DemoPage</title>
+</head>
+<body>
+<p>FFFFFFFF!</p>
+</body>
+</html>
